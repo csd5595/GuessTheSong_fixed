aa3165c59dd4b117ef8d10d4eae0a69e64527650
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 21:33:41 +0300

    tetsing sound again

--- playlist.js
+++ playlist.js
@@ -1,8 +1,16 @@
 const mySongs = [
     { 
-        title: "I took a pill in Ibiza", 
+        title: "I Took a Pill in Ibiza", 
         artist: "Mike Posner", 
-        // I changed 'www' to 'dl.dropboxusercontent' below:
-        url: "https://dl.dropboxusercontent.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1"
+        // I swapped 'www' for 'dl.dropboxusercontent' for you here:
+        url: "https://dl.dropboxusercontent.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1" 
     }
+    /* To add more songs, add a comma after the last one and repeat the block:
+    ,
+    {
+        title: "Song 2",
+        artist: "Artist 2",
+        url: "PASTE_LINK_HERE_AND_CHANGE_TO_DL.DROPBOXUSERCONTENT"
+    }
+    */
 ];
