f4412387d53d84c06d2fb715dde7c5733caf864a
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 21:26:25 +0300

    testing

--- playlist.js
+++ playlist.js
@@ -2,6 +2,6 @@ const mySongs = [
     { 
         title: "I took a pill in Ibiza", 
         artist: "idk girl", 
-        url: "https://www.dropbox.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=0" // Use the exact filename!
+        url: "https://www.dropbox.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1" // Use the exact filename!
     }
 ];
