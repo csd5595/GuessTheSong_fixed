0475ffaf9dd76f1a3ca615233105e815640665e3
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 21:30:54 +0300

    tetsing still

--- playlist.js
+++ playlist.js
@@ -1,17 +1,8 @@
 const mySongs = [
     { 
         title: "I took a pill in Ibiza", 
-        artist: "idk girl", 
-        url: "https://www.dropbox.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1" // Use the exact filename!
+        artist: "Mike Posner", 
+        // I changed 'www' to 'dl.dropboxusercontent' below:
+        url: "https://dl.dropboxusercontent.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1"
     }
-
 ];
-function startTurn() {
-    currentSong = mySongs[Math.floor(Math.random() * mySongs.length)];
-    audio.src = currentSong.url;
-    
-    audio.load(); // ADD THIS LINE
-    audio.play(); 
-    
-    // ... rest of your timer code ...
-}
