0401e8799d006ec56f590c4a172c4f5ad5561376
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 21:28:47 +0300

    test

--- playlist.js
+++ playlist.js
@@ -4,4 +4,14 @@ const mySongs = [
         artist: "idk girl", 
         url: "https://www.dropbox.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1" // Use the exact filename!
     }
+
 ];
+function startTurn() {
+    currentSong = mySongs[Math.floor(Math.random() * mySongs.length)];
+    audio.src = currentSong.url;
+    
+    audio.load(); // ADD THIS LINE
+    audio.play(); 
+    
+    // ... rest of your timer code ...
+}
