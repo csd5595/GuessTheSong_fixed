45a3179171a0c975e9e3cc37d9819e911ca96eb7
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 21:08:31 +0300

    test song

--- playlist.js
+++ playlist.js
@@ -1,7 +1,7 @@
 const mySongs = [
     { 
-        title: "Waiting for Music", 
-        artist: "Upload to Dropbox First!", 
-        url: "" 
+        title: "I took a pill in Ibiza", 
+        artist: "idk girl", 
+        url: "songlist/i took a pill in ibiza.m4a" // Use the exact filename!
     }
 ];
