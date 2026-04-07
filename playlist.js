1153747408b10bb805a9183b60dfea51facc69fa
csd5595 <csd5595@csd.uoc.gr>
2026-04-07 22:25:05 +0300

    added a couple songs for testiing

--- playlist.js
+++ playlist.js
@@ -5,12 +5,17 @@ const mySongs = [
         // I swapped 'www' for 'dl.dropboxusercontent' for you here:
         url: "https://dl.dropboxusercontent.com/scl/fi/400idathjtc406osp2tp1/idiza.m4a?rlkey=c90fhtnynmsvql76rig7hyw6m&dl=1" 
     }
-    /* To add more songs, add a comma after the last one and repeat the block:
     ,
     {
-        title: "Song 2",
-        artist: "Artist 2",
-        url: "PASTE_LINK_HERE_AND_CHANGE_TO_DL.DROPBOXUSERCONTENT"
+        title: "Babydoll",
+        artist: "Dominic Fike",
+        url: "https://dl.dropboxusercontent.com/scl/fi/ugbdv43yamlfqc2lc18hg/babydoll.m4a?rlkey=w77n4jslmx34rzz5sr3mv5why&dl=1"
+    },
+    {
+        title: "Love The Way You Lie",
+        artist: "Eminem ft. Rihanna",
+        url: "https://dl.dropboxusercontent.com/scl/fi/izkri9tb5ybti58jqgi65/i_love_the_way_you_lie.m4a?rlkey=6hne5n7gml6mj8ihryz39p6gs&dl=1"
     }
-    */
+
+
 ];
